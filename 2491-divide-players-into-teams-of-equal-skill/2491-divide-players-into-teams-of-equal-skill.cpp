bfe9c8e42befea44cec8c165ff23989e69d37d3e
Anushka Kawalkar <anushkakawalkar18@gmail.com>
2026-09-16 00:19:13 +0530

    Time: 11 ms (85.09%), Space: 56.3 MB (98.26%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.cpp
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     long long dividePlayers(vector<int>& skill) {
-        sort(skill.begin(), skill.end());
-
-        int left = 0;
-        int right = skill.size() - 1;
-
-        int target = skill[left] + skill[right];
-        long long chemistry = 0;
-
-        while (left < right) {
-            // Every pair must have the same total skill
-            if (skill[left] + skill[right] != target) {
+        sort(skill.begin(),skill.end());
+        int left=0;
+        int right=skill.size()-1;
+        long long chemistry=0;
+        int targetskill=skill[left]+skill[right];
+        while(left<right){
+            if(skill[left]+skill[right]!=targetskill){
                 return -1;
             }
-
-            chemistry += (long long)skill[left] * skill[right];
-
-            left++;
-            right--;
+        chemistry+=skill[left]*skill[right];
+        left++;
+        right--;
+        
         }
-
         return chemistry;
     }
 };
