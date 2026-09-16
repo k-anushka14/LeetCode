e3d8470395ff82526050f52beffc94e819dea09d
Anushka Kawalkar <anushkakawalkar18@gmail.com>
2026-09-17 01:18:13 +0530

    Time: 12 ms (85.08%), Space: 45.8 MB (71.28%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int left=0;
+        int right=people.size()-1;
+        int boats=0;
+        while(left<=right){
+            if(people[left]+people[right]<=limit){
+                //lighest and heaviest can share
+                left++;
+                right--;
+            }
+            else{
+                right--;
+                //heaviest must go alone
+            }
+            boats++;
+
+        }
+        return boats;
+    }
+};
