ca34eca0493e672896231eedc593f537c547d279
Anushka Kawalkar <anushkakawalkar18@gmail.com>
2026-09-19 02:19:27 +0530

    Time: 51 ms (41.35%), Space: 29.2 MB (45.9%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,45 +1,55 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
 
+        vector<vector<int>> answer;
+
+        // Step 1: Sort the array
         sort(nums.begin(), nums.end());
 
-        for (int i = 0; i < nums.size() - 2; i++) {
+        // Step 2: Fix one number
+        for(int i = 0; i < nums.size(); i++) {
 
-            // Skip duplicate first elements
-            if (i > 0 && nums[i] == nums[i - 1])
+            // Skip duplicate values for i
+            if(i > 0 && nums[i] == nums[i - 1])
                 continue;
 
             int left = i + 1;
             int right = nums.size() - 1;
 
-            while (left < right) {
+            // Step 3: Two pointer approach
+            while(left < right) {
 
                 int sum = nums[i] + nums[left] + nums[right];
 
-                if (sum == 0) {
-                    ans.push_back({nums[i], nums[left], nums[right]});
-
+                if(sum < 0) {
                     left++;
-                    right--;
-
-                    // Skip duplicates
-                    while (left < right && nums[left] == nums[left - 1])
-                        left++;
-
-                    while (left < right && nums[right] == nums[right + 1])
-                        right--;
                 }
-                else if (sum < 0) {
-                    left++;
+                else if(sum > 0) {
+                    right--;
                 }
                 else {
+                    // We found a triplet
+                    answer.push_back({
+                        nums[i],
+                        nums[left],
+                        nums[right]
+                    });
+
+                    left++;
                     right--;
+
+                    // Skip duplicate left values
+                    while(left < right && nums[left] == nums[left - 1])
+                        left++;
+
+                    // Skip duplicate right values
+                    while(left < right && nums[right] == nums[right + 1])
+                        right--;
                 }
             }
         }
 
-        return ans;
+        return answer;
     }
 };
